ef59cb20355e187d4e5a00774bb6965c3d80d1a5
Eric Vela <eric.vela2002@gmail.com>
2023-10-18 03:39:18 -0400

    Theme basically done

--- src/components/ChooseBackground.jsx
+++ src/components/ChooseBackground.jsx
@@ -14,8 +14,14 @@ function ChooseBackground(props) {
     return (
         <div className='choose-background-container' style={props.open ? { bottom: 0 } : {}}>
             <div className='choose-background-options'>
-                <button className="choose-background-button white" style={{background: "#CBC9BC"}} onClick={() => handleChooseTheme("light")}/>
-                <button className="choose-background-button black" style={{background: "#0D0D0D"}} onClick={() => handleChooseTheme("dark")}/>
+                <div className='choose-background-button-container'>
+                    <p className='choose-background-button-label'>Light</p>
+                    <button className="choose-background-button white" style={{background: "#CBC9BC"}} onClick={() => handleChooseTheme("light")} />
+                </div>
+                <div className='choose-background-button-container'>
+                    <p className='choose-background-button-label'>Dark</p>
+                    <button className="choose-background-button black" style={{background: "#0D0D0D"}} onClick={() => handleChooseTheme("dark")} />
+                </div>
             </div>
             <button className='choose-background-button' style={{ width: "5rem" }} onClick={toggleClose}>Close</button>
         </div>
